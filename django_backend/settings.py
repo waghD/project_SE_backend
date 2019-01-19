a025bbfa69834576cd890ce52d013934dfa99bf2
vambi <simon.vamberszky@live.de>
2019-01-19 10:27:39 +0100

    hotfix

--- django_backend/settings.py
+++ django_backend/settings.py
@@ -125,6 +125,10 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     )
